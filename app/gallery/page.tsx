9357572f53c74ae747a93bf2febe3d2581903189
Luis Anaya <brothersailorwebdev@gmail.com>
2023-09-05 02:18:42 +0200

    added pagination

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next"
+import Link from "next/link"
 import { SignedIn, SignedOut } from "@clerk/nextjs"
+import { sql } from "drizzle-orm"
+import {
+  ChevronLeftIcon,
+  ChevronRightCircle,
+  ChevronRightIcon,
+} from "lucide-react"
 
 import { db } from "@/lib/db"
 import { galleryImages } from "@/lib/db/schema"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 import { SignInBtn } from "@/components/buttons/sign-in-btn"
 import { SignOutBtn } from "@/components/buttons/sign-out-btn"
 import { ImageModal } from "@/components/gallery/image-modal"
@@ -12,9 +21,30 @@ export const metadata: Metadata = {
   title: "Gallery",
 }
 
-export default async function GalleryPage() {
-  const imgs = await db.select().from(galleryImages)
+interface GalleryPageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
 
+export default async function GalleryPage({ searchParams }: GalleryPageProps) {
+  const page =
+    typeof searchParams.page === "string" ? Number(searchParams.page) : 1
+
+  // amount of documents you want to return in the query
+  const limit =
+    typeof searchParams.limit === "string" ? Number(searchParams.limit) : 10
+
+  // amount of documents you want to skip
+  const skip = (page - 1) * limit
+
+  const imgs = await db.select().from(galleryImages).limit(limit).offset(skip)
+
+  const totalImgs = await db
+    .select({ count: sql<number>`count(*)` })
+    .from(galleryImages)
+
+  const totalPages = Math.ceil(totalImgs[0].count / limit)
   return (
     <main className="container py-36">
       <section className="flex flex-col items-center justify-center gap-y-5">
@@ -33,10 +63,48 @@ export default async function GalleryPage() {
 
       <section className="mt-20">
         {imgs && imgs.length ? (
-          <div className="columns-1 gap-5 sm:columns-1 lg:columns-2 lg:gap-5 xl:columns-3 [&>div:not(:first-child)]:mt-5">
-            {imgs?.map((img, imgIdx) => (
-              <ImageModal img={img} key={imgIdx} idx={imgIdx} />
-            ))}
+          <div className="flex flex-col gap-y-5">
+            <div className="flex items-center gap-x-2.5 md:gap-x-5">
+              <Link
+                href={`/gallery?page=${page > 1 ? page - 1 : 1}`}
+                // // href={{
+                // //   pathname: '/movies',
+                // //   query: {
+                // //     ...(search ? { search } : {}),
+                // //     page: page > 1 ? page - 1 : 1
+                // //   }
+                // // }}
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "icon" }),
+                  page <= 1 && "pointer-events-none opacity-50"
+                )}
+              >
+                <ChevronLeftIcon className="h-4 w-4" />
+              </Link>
+
+              <div className="tabular-nums">{`${page} of ${totalPages}`}</div>
+              <Link
+                href={`/gallery?page=${page + 1}`}
+                // // href={{
+                // //   pathname: '/movies',
+                // //   query: {
+                // //     ...(search ? { search } : {}),
+                // //     page: page + 1
+                // //   }
+                // // }}
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "icon" }),
+                  imgs.length < 10 && "pointer-events-none opacity-50"
+                )}
+              >
+                <ChevronRightIcon className="h-4 w-4" />
+              </Link>
+            </div>
+            <div className="columns-1 gap-5 sm:columns-2 lg:columns-3 lg:gap-5 xl:columns-4 [&>div:not(:first-child)]:mt-5">
+              {imgs?.map((img, imgIdx) => (
+                <ImageModal img={img} key={imgIdx} idx={imgIdx} />
+              ))}
+            </div>
           </div>
         ) : (
           <div className="mx-auto max-w-3xl text-center">
